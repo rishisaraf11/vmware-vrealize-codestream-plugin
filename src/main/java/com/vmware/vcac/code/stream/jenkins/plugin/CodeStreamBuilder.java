150873989af6c8da376c6d67cfacd15e349f561e
Rishi <rsaraf@vmware.com>
2015-03-18 23:23:29 +0530

    CodeStream config fields

--- src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/CodeStreamBuilder.java
+++ src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/CodeStreamBuilder.java
@@ -1,4 +1,5 @@
-package com.vmware.vcac.codestreamjenkinsplugin;
+package com.vmware.vcac.code.stream.jenkins.plugin;
+
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.util.FormValidation;
@@ -17,105 +18,102 @@ import java.io.IOException;
 
 /**
  * Sample {@link Builder}.
- *
- * <p>
+ * <p/>
+ * <p/>
  * When the user configures the project and enables this builder,
  * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
  * and a new {@link CodeStreamBuilder} is created. The created
  * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
+ * XStream, so this allows you to use instance fields (like {@link })
  * to remember the configuration.
- *
- * <p>
+ * <p/>
+ * <p/>
  * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
- * method will be invoked. 
+ * method will be invoked.
  *
  * @author Rishi Saraf
  */
 public class CodeStreamBuilder extends Builder {
 
-    private final String name;
+    private String serverUrl;
+    private String userName;
+    private String password;
+    private String tenant;
+    private String pipelineName;
 
     // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public CodeStreamBuilder(String name) {
-        this.name = name;
+    public CodeStreamBuilder(String serverUrl, String userName, String password, String tenant, String pipelineName) {
+        this.serverUrl = serverUrl;
+        this.userName = userName;
+        this.password = password;
+        this.tenant = tenant;
+        this.pipelineName = pipelineName;
+    }
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getTenant() {
+        return tenant;
+    }
+
+    public String getPipelineName() {
+        return pipelineName;
     }
 
     /**
      * We'll use this from the <tt>config.jelly</tt>.
      */
-    public String getName() {
-        return name;
-    }
 
-    @Override
-    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        // This is where you 'build' the project.
-        // Since this is a dummy, we just say 'hello world' and call that a build.
-
-        // This also shows how you can consult the global configuration of the builder
-        if (getDescriptor().getUseFrench())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
-        return true;
-    }
 
     // Overridden for better type safety.
     // If your plugin doesn't really define any property on Descriptor,
     // you don't have to do this.
     @Override
     public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl)super.getDescriptor();
+        return (DescriptorImpl) super.getDescriptor();
     }
 
     /**
      * Descriptor for {@link CodeStreamBuilder}. Used as a singleton.
      * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
+     * <p/>
+     * <p/>
      * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
      * for the actual HTML fragment for the configuration screen.
      */
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
         /**
          * To persist global configuration information,
          * simply store it in a field and call save().
-         *
-         * <p>
+         * <p/>
+         * <p/>
          * If you don't want fields to be persisted, use <tt>transient</tt>.
          */
-        private boolean useFrench;
+        private String serverUrl;
+        private String userName;
+        private String password;
+        private String tenant;
+        private String pipelineName;
 
         /**
-         * In order to load the persisted global configuration, you have to 
+         * In order to load the persisted global configuration, you have to
          * call load() in the constructor.
          */
         public DescriptorImpl() {
             load();
-        }
-
-        /**
-         * Performs on-the-fly validation of the form field 'name'.
-         *
-         * @param value
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
-         *      <p>
-         *      Note that returning {@link FormValidation#error(String)} does not
-         *      prevent the form from being saved. It just means that a message
-         *      will be displayed to the user. 
-         */
-        public FormValidation doCheckName(@QueryParameter String value)
-                throws IOException, ServletException {
-            if (value.length() == 0)
-                return FormValidation.error("Please set a name");
-            if (value.length() < 4)
-                return FormValidation.warning("Isn't the name too short?");
-            return FormValidation.ok();
         }
 
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
@@ -127,28 +125,42 @@ public class CodeStreamBuilder extends Builder {
          * This human readable name is used in the configuration screen.
          */
         public String getDisplayName() {
-            return "Say hello world";
+            return "Execute CodeStream Pipeline";
         }
 
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             // To persist global configuration information,
             // set that to properties and call save().
-            useFrench = formData.getBoolean("useFrench");
             // ^Can also use req.bindJSON(this, formData);
             //  (easier when there are many fields; need set* methods for this, like setUseFrench)
+            serverUrl = formData.getString("serverUrl");
+            tenant = formData.getString("tenant");
+            userName = formData.getString("userName");
+            password = formData.getString("password");
+            pipelineName = formData.getString("pipelineName");
             save();
-            return super.configure(req,formData);
+            return super.configure(req, formData);
         }
 
-        /**
-         * This method returns true if the global configuration says we should speak French.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
-         */
-        public boolean getUseFrench() {
-            return useFrench;
+        public String getServerUrl() {
+            return serverUrl;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getTenant() {
+            return tenant;
+        }
+
+        public String getPipelineName() {
+            return pipelineName;
         }
     }
 }
