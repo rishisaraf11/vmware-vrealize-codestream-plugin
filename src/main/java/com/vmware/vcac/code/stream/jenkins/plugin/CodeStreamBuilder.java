3b9cf201e1ee2d59fd4743e5141536a288501748
Rishi <rsaraf@vmware.com>
2015-03-19 11:20:36 +0530

    Execute pipeline on jenkins build

--- src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/CodeStreamBuilder.java
+++ src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/CodeStreamBuilder.java
@@ -1,20 +1,35 @@
 package com.vmware.vcac.code.stream.jenkins.plugin;
 
-import hudson.Launcher;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import hudson.Extension;
-import hudson.util.FormValidation;
+import hudson.Launcher;
 import hudson.model.AbstractBuild;
-import hudson.model.BuildListener;
 import hudson.model.AbstractProject;
-import hudson.tasks.Builder;
+import hudson.model.BuildListener;
 import hudson.tasks.BuildStepDescriptor;
-import net.sf.json.JSONObject;
+import hudson.tasks.Builder;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.QueryParameter;
 
-import javax.servlet.ServletException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Sample {@link Builder}.
@@ -69,6 +84,116 @@ public class CodeStreamBuilder extends Builder {
 
     public String getPipelineName() {
         return pipelineName;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+        PrintStream logger = listener.getLogger();
+        SSLContextBuilder builder = new SSLContextBuilder();
+        CloseableHttpClient httpClient = null;
+        try {
+            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                    builder.build());
+
+            httpClient = HttpClients.custom().setSSLSocketFactory(
+                    sslsf).build();
+
+//            String identityTokenUrl = "https://blr-v144-ip-156.eng.vmware.com" + "/identity/api/tokens";
+            String identityTokenUrl = this.serverUrl + "/identity/api/tokens";
+            HttpPost postRequest  = new HttpPost(identityTokenUrl);
+            String tokenPayload = String.format("{\"username\": \"%s\", \"password\": \"%s\", \"tenant\": \"%s\"}", userName, password, tenant);
+//            StringEntity input = new StringEntity("{\"username\": \"fritz@coke.com\", \"password\": \"password\", \"tenant\": \"qe\"}");
+            StringEntity input = new StringEntity(tokenPayload);
+
+            input.setContentType("application/json");
+
+            postRequest.setEntity(input);
+            postRequest.setHeader("Content-Type", "application/json");
+            HttpResponse response = httpClient.execute(postRequest);
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                output.append(line);
+            }
+
+
+            Gson gson = new Gson();
+            JsonElement jsonElement = new JsonParser().parse(output.toString());
+            JsonObject asJsonObject = jsonElement.getAsJsonObject();
+            String token = asJsonObject.get("id").getAsString();
+
+            logger.println("Token :" + token);
+
+            String fetchPipeline = this.serverUrl + "/release-management-service/api/release-pipelines/?name=" + this.pipelineName ;
+
+            HttpGet request = new HttpGet(fetchPipeline);
+            request.setHeader("accept", "application/json; charset=utf-8");
+            String autorization = "Bearer " + token;
+            request.setHeader("Authorization", autorization);
+            response = httpClient.execute(request);
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            StringBuffer resultBuffer = new StringBuffer();
+            line = "";
+            while ((line = rd.readLine()) != null) {
+                resultBuffer.append(line);
+            }
+
+            logger.println("Pipeline :" + resultBuffer);
+
+            jsonElement = new JsonParser().parse(resultBuffer.toString());
+            String pipelineId = jsonElement.getAsJsonObject().get("content").getAsJsonArray().get(0).getAsJsonObject().get("id").getAsString();
+
+
+            // Execute pipeline
+            CloseableHttpClient newClient =  HttpClients.custom().setSSLSocketFactory(
+                    sslsf).build();
+            String executePipelineUrl = this.serverUrl + "/release-management-service/api/release-pipelines/" + pipelineId + "/executions";
+            HttpPost executePostRequest  = new HttpPost(executePipelineUrl);
+            String payload = String.format("{\"description\": \"%s\"}", "Executed from jenkins");
+            StringEntity inputEntity = new StringEntity(payload);
+            inputEntity.setContentType("application/json");
+            executePostRequest.setEntity(inputEntity);
+            executePostRequest.setHeader("accept", "application/json; charset=utf-8");
+            executePostRequest.setHeader("Authorization", autorization);
+
+            newClient.execute(executePostRequest);
+
+
+//            BufferedReader bre = new BufferedReader(
+//                    new InputStreamReader((executeResponse.getEntity().getContent())));
+//
+//            StringBuilder outputE = new StringBuilder();
+//            String linee;
+//            while ((linee = bre.readLine()) != null) {
+//                outputE.append(linee);
+//            }
+//
+//            logger.println("Execution response :" + outputE);
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            httpClient.close();
+        }
+
+
+
+
+
+        return true;
     }
 
     /**
@@ -129,7 +254,7 @@ public class CodeStreamBuilder extends Builder {
         }
 
         @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+        public boolean configure(StaplerRequest req, net.sf.json.JSONObject formData) throws FormException {
             // To persist global configuration information,
             // set that to properties and call save().
             // ^Can also use req.bindJSON(this, formData);
