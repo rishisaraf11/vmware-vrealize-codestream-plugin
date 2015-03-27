1e9efdeb17bcb6a0f414b51dcdadcbe37b6eee1a
Rishi <rishisaraf11@gmail.com>
2015-03-27 23:15:24 +0530

    test commit

--- src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/PipelineParam.java
+++ src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/PipelineParam.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @ExportedBean
 public class PipelineParam extends AbstractDescribableImpl<PipelineParam> implements Serializable {
     private String name;
+
     private String type="STRING";
     private String description="";
     private String value;
