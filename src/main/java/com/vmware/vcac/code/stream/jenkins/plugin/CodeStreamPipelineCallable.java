605eb7c6ae0e35eeea442eb4feffa6e545528b3e
Rishi <rishisaraf11@gmail.com>
2015-04-20 17:41:51 +0530

    pipeline execution o/p

--- src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/CodeStreamPipelineCallable.java
+++ src/main/java/com/vmware/vcac/code/stream/jenkins/plugin/CodeStreamPipelineCallable.java
@@ -75,10 +75,10 @@ public class CodeStreamPipelineCallable implements Callable<Map<String, String>,
                     JsonObject pipelineExecutionResponse = codeStreamClient.getPipelineExecutionResponse(pipelineId, execId);
                     ExecutionStatus pipelineExecStatus = codeStreamClient.getPipelineExecStatus(pipelineExecutionResponse);
                     data.put("CS_PIPELINE_EXECUTION_STATUS", pipelineExecStatus.toString());
+                    data.put("CS_PIPELINE_EXECUTION_RES", pipelineExecutionResponse.toString());
                     switch (pipelineExecStatus) {
                         case COMPLETED:
                             System.out.println("Pipeline complete successfully");
-                            data.put("CS_PIPELINE_EXECUTION_RES", pipelineExecutionResponse.toString());
                             break;
                         case FAILED:
                             System.out.println("Pipeline execution failed");
